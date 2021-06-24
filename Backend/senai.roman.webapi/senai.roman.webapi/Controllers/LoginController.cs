f0d54ae166ef080d2fc0d8783f4831135ddd8bfd
Mateus Rodrigues Silva <73179530+Mateusrodrigues75@users.noreply.github.com>
2021-06-24 10:03:51 -0300

    API Finalizada // Collection Postman add

---
+++ Backend/senai.roman.webapi/senai.roman.webapi/Controllers/LoginController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using senai.roman.webapi.Domains;
+using senai.roman.webapi.Interfaces;
+using senai.roman.webapi.Repositories;
+using senai.roman.webapi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace senai.roman.webapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        public LoginController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        /// <summary>
+        /// Valida o usuário
+        /// </summary>
+        /// <param name="Login">Objeto login com o e-mail e a senha do usuário</param>
+        /// <returns>Token com as informações do usuário</returns>
+        /// dominio/api/Login
+        [HttpPost]
+        public IActionResult Post(LoginViewModel Login)
+        {
+            try
+            {
+                Usuario UsuarioBuscado = _usuarioRepository.Login(Login.Email, Login.Senha);
+
+                if (UsuarioBuscado == null)
+                {
+                    return NotFound("E-mail ou senha inválidos!");
+                }
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, UsuarioBuscado.Email),
+
+                    new Claim(JwtRegisteredClaimNames.Jti, UsuarioBuscado.IdUsuario.ToString()),
+
+                    new Claim(ClaimTypes.Role, UsuarioBuscado.IdTipoUsuario.ToString()),
+
+                    new Claim(ClaimTypes.Name, UsuarioBuscado.NomeUsuario),
+
+                    new Claim("role", UsuarioBuscado.IdTipoUsuario.ToString())
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("roman-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "roman.webApi",
+                    audience: "roman.webApi",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds
+                );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
--- Backend/senai.roman.webapi/senai.roman.webapi/Repositories/ProjetoRepository.cs
+++ Backend/senai.roman.webapi/senai.roman.webapi/Repositories/ProjetoRepository.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using senai.roman.webapi.Contexts;
+using senai.roman.webapi.Domains;
+using senai.roman.webapi.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace senai.roman.webapi.Repositories
 {
-    public class ProjetoRepository
+    public class ProjetoRepository : IProjetoRepository
     {
+        RomanContext ctx = new RomanContext();
+
+        public void Atualizar(int id, Projeto ProjetoAtt)
+        {
+            Projeto ProjetoBuscado = ctx.Projetos.Find(id);
+
+            if (ProjetoAtt.NomeProjeto != null)
+            {
+                ProjetoBuscado.NomeProjeto = ProjetoAtt.NomeProjeto;
+            }
+
+            ctx.Projetos.Update(ProjetoBuscado);
+            ctx.SaveChanges();
+        }
+
+        public Projeto BuscarPorId(int id)
+        {
+            return ctx.Projetos.FirstOrDefault(t => t.IdProjeto == id);
+
+        }
+
+        public void Cadastrar(Projeto NovoProjeto)
+        {
+            ctx.Projetos.Add(NovoProjeto);
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Projeto ProjetoBuscado = ctx.Projetos.Find(id);
+
+            ctx.Projetos.Remove(ProjetoBuscado);
+            ctx.SaveChanges();
+        }
+
+        public List<Projeto> Listar()
+        {
+            return ctx.Projetos.ToList();
+        }
     }
 }
